e9beb78374090c99f66e5617354c1ffeab904f4d
Sergey059 <sergeiv-82@mail.ru>
2021-08-03 20:15:25 +0500

    Starter template for site layout

---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+let fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
+
+const { src, dest, parallel, series, watch } = require('gulp')
+const browserSync  = require('browser-sync').create()
+const bssi         = require('browsersync-ssi')
+const ssi          = require('ssi')
+const webpack      = require('webpack-stream')
+const pug          = require('gulp-pug')
+const sass         = require('gulp-sass')(require('sass'))
+const sassglob     = require('gulp-sass-glob')
+const cleancss     = require('gulp-clean-css')
+const autoprefixer = require('gulp-autoprefixer')
+const rename       = require('gulp-rename')
+const imagemin     = require('gulp-imagemin')
+const newer        = require('gulp-newer')
+const rsync        = require('gulp-rsync')
+const del          = require('del')
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/',
+			middleware: bssi({ baseDir: 'app/', ext: '.html' })
+		},
+		ghostMode: { clicks: false },
+		notify: false,
+		online: true,
+		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+	})
+}
+
+function scripts() {
+	return src(['app/js/*.js', '!app/js/*.min.js'])
+		.pipe(webpack({
+			mode: 'production',
+			performance: { hints: false },
+			module: {
+				rules: [
+					{
+						test: /\.(js)$/,
+						exclude: /(node_modules)/,
+						loader: 'babel-loader',
+						query: {
+							presets: ['@babel/env'],
+							plugins: ['babel-plugin-root-import']
+						}
+					}
+				]
+			}
+		})).on('error', function handleError() {
+			this.emit('end')
+		})
+		.pipe(rename('app.min.js'))
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+}
+
+function styles() {
+	return src([`app/sass/*.*`, `!app/sass/_*.*`])
+		.pipe(sassglob())
+		.pipe(sass())
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+		.pipe(cleancss({ level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
+}
+
+function html() {
+	return src(['app/pug/*.*', '!app/pug/_*.pug'])
+		.pipe(pug({pretty: true}))
+		.pipe(dest('./app'))
+		.pipe(browserSync.stream());
+}
+
+function images() {
+	return src(['app/images/src/**/*'])
+		.pipe(newer('app/images/dist'))
+		.pipe(imagemin())
+		.pipe(dest('app/images/dist'))
+		.pipe(browserSync.stream())
+}
+
+function buildcopy() {
+	return src([
+		'{app/js,app/css}/*.min.*',
+		'app/images/**/*.*',
+		'!app/images/src/**/*',
+		'app/fonts/**/*'
+	], { base: 'app/' })
+	.pipe(dest('dist'))
+}
+
+async function buildhtml() {
+	let includes = new ssi('app/', 'dist/', '/**/*.html')
+	includes.compile()
+	// del('dist/parts', { force: true })
+}
+
+function cleandist() {
+	return del('dist/**/*', { force: true })
+}
+
+function deploy() {
+	return src('dist/')
+		.pipe(rsync({
+			root: 'dist/',
+			hostname: 'username@yousite.com',
+			destination: 'yousite/public_html/',
+			// clean: true, // Mirror copy with file deletion
+			include: [/* '*.htaccess' */], // Included files to deploy,
+			exclude: [ '**/Thumbs.db', '**/*.DS_Store' ],
+			recursive: true,
+			archive: true,
+			silent: false,
+			compress: true
+		}))
+}
+
+function startwatch() {
+	watch(['app/sass/**/*', 'app/components/**/*'], { usePolling: true }, styles)
+	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
+	watch(['app/pug/**/*.*', 'app/components/**/*.*'], { usePolling: true }, html)
+	watch('app/images/src/**/*.{jpg,jpeg,png,webp,svg,gif}', { usePolling: true }, images)
+	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
+}
+
+exports.scripts = scripts
+exports.styles  = styles
+exports.html    = html
+exports.images  = images
+exports.deploy  = deploy
+exports.assets  = series(scripts, styles, html, images)
+exports.build   = series(cleandist, scripts, styles, html, images, buildcopy, buildhtml)
+exports.default = series(scripts, styles, html, images, parallel(browsersync, startwatch))
---
+++ readme.md
@@ -0,0 +1,19 @@
+## Starter template for site layout
+
+SaSS, PUG, Autoprefixer, Webpack-stream, Babel, Image optimization 
+
+### Настройка и запуск проекта
+
+Проверка обновлений - "ncu"
+
+Обновление пакетов в package.json - "ncu -u"
+
+Установка пакетов - "npm install"
+
+Запуск сборки - "gulp" или "npm start"
+
+Build проекта - "gulp build" (в папке dist готовая верстка)
+
+***
+
+>PS. Огромная благодарность [Алексею](https://github.com/agragregra)
